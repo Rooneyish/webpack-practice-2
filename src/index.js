a4ad92202f05d1ecc07d4047b572936c213d88ae
rooneyish <ilustraterp@gmail.com>
2025-01-28 12:06:10 +0545

    Loading Webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 import odinImage from "./odin.png";
+import sceneImage from "./a.png";
 
 const image= document.createElement("img");
 image.src=odinImage;
+image.src=sceneImage;
 
 document.body.appendChild(image);
-
 console.log(greeting);
